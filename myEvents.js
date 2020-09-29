1de992b362dfc4f5ef584e4ac7f960249302ba21
Thomas Nguyen <tpnguyen1999@gmail.com>
2020-09-29 00:24:48 -0500

    Attempt Grid, Attempt onHoverCommands

---
+++ myEvents.js
@@ -0,0 +1,13 @@
+table_element = document.getElementsByClassName("table_element");
+
+//table_element.addEventListener("mouseover", mouseOver);
+//table_element.addEventListener("")
+
+function mouseOver() {
+    var img = document.getElementById("tiny_img");
+    img.style.visibility = "visible";
+}
+
+function mouseOut() {
+    
+}
