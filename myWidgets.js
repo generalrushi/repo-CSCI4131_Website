74a684ead00ccfb713b5ba9fcfd5905827b883f3
Thomas Nguyen <tpnguyen1999@gmail.com>
2020-09-30 17:34:57 -0500

    Attempted to make changes to Widget page, and made small changes to event page

--- myWidgets.js
+++ myWidgets.js
@@ -1,44 +1,63 @@
+var myInput = document.getElementById("password");
+var result = document.getElementById("result");
+var myProgressBar = document.getElementById("progressBar");
+var progress = myProgressBar.style.width;
+
+myInput.onkeyup = function() {
+    var passStrength = checkPassword(myInput);
+    result.value = passStrength;
+}
+
 function checkPassword(password) 
 {
     var strength = 0;
-    if (password.length < 8)
+    console.log(strength);
+    if (password.value.length < 8)
     {
         document.getElementById("result").innerHTML = "Too Short..."
     }
-    if (password.length > 7)
+    // contains more than 7 characters
+    if (myInput.value.length > 7)
     {
         strength += 1;
+        progress += 20 + "%";
+        console.log(strength);
     }
-    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) 
+    // contains upper and lower case values
+    if (myInput.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) 
     {
         strength += 1;
+        progress += 20 + "%";
     }
+    // number + characters
     if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))
     {
         strength += 1;
     }
+    // contains a special character
     if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))
     {
         strength += 1;
     }
+    // contains 2 special characters
     if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/))
     {
         strength += 1;
     }
     if (strength < 2)
     {
-        document.getElementById("result").innerHTML = "Weak";
+        return "Weak";
     }
     else if (strength < 3)
     {
-        document.getElementById("result").innerHTML = "Decent";
+        return "Decent";
     }
     else if (strength < 4)
     {
-        document.getElementById("result").innerHTML = "Good";
+        return "Good";
     }
     else
     {
-        document.getElementById("result").innerHTML = "Strong";
+        return "Strong";
     }
 }
