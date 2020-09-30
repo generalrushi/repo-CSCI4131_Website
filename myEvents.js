14d289ff9f4fd454cdd869278ebcf345a4728f03
Thomas Nguyen <tpnguyen1999@gmail.com>
2020-09-30 00:35:54 -0500

    Completed myEvents Requirements

--- myEvents.js
+++ myEvents.js
@@ -1,13 +1,14 @@
 table_element = document.getElementsByClassName("table_element");
 
 //table_element.addEventListener("mouseover", mouseOver);
-//table_element.addEventListener("")
 
-function mouseOver() {
-    var img = document.getElementById("tiny_img");
-    img.style.visibility = "visible";
+function mouseOver(element) {
+    var img = document.getElementById("enlarged_img");
+    var location = element.children[3];
+    img.src = location.children[location.children.length - 1].src
 }
 
 function mouseOut() {
-    
+    var img = document.getElementByClassName("tiny_img");
+    img.style.visibility = "hidden";
 }
